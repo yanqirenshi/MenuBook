eaeb6ced9cfc8a9df26ac4abaff063876bffa0ab
yanqirenshi <yanqirenshi@gmail.com>
2023-12-10 15:22:43 +0900

    add dist/

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _MenuBook = _interopRequireDefault(require("MenuBook.js"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var _default = exports["default"] = _MenuBook["default"];
